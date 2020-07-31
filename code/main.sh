992984947c7d7adf30ef5fb790500c1ec7e31160
bartala <bartala@gmail.com>
2020-07-31 12:27:27 -0400

    Create main.sh

---
+++ code/main.sh
@@ -0,0 +1,9 @@
+python3 '/home/maayanlab/enrichrbot/covid/collect_restapi.py'
+
+python3 '/home/maayanlab/enrichrbot/covid/process_tweets.py'
+
+Rscript '/home/maayanlab/enrichrbot/covid/analyze.r'
+
+python3 '/home/maayanlab/enrichrbot/covid/post_tweet.py'
+
+python3 '/home/maayanlab/enrichrbot/covid/push_data.py'
