6e5538dbcbec2d14bec1fe5368d62e724e49eb17
bartala <bartala@gmail.com>
2020-07-31 12:43:48 -0400

    Update main.sh

--- code/main.sh
+++ code/main.sh
@@ -1,9 +1,9 @@
-python3 '/home/maayanlab/enrichrbot/covid/collect_restapi.py'
+python3 '.../covid/collect_restapi.py'
 
-python3 '/home/maayanlab/enrichrbot/covid/process_tweets.py'
+python3 '.../covid/process_tweets.py'
 
-Rscript '/home/maayanlab/enrichrbot/covid/analyze.r'
+Rscript '.../covid/analyze.r'
 
-python3 '/home/maayanlab/enrichrbot/covid/post_tweet.py'
+python3 '.../covid/post_tweet.py'
 
-python3 '/home/maayanlab/enrichrbot/covid/push_data.py'
+python3 '.../covid/push_data.py'
